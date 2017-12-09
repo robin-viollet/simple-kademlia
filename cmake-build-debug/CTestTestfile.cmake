fc797c95800a0ddd39051f87e74793f7bed71a6a
Sierra Kaplan-Nelson <sierrakn@stanford.edu>
2017-12-09 12:35:19 -0800

    removed .idea and cmake build folders

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sierrakaplan-nelson/Documents/COLLEGE/Fall2017/CS244B/simple-kademlia
-# Build directory: /Users/sierrakaplan-nelson/Documents/COLLEGE/Fall2017/CS244B/simple-kademlia/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
